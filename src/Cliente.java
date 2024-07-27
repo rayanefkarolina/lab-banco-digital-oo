55e5179fd877ffa0527b1a99828e4f54bdacec91
rayanefkarolina <rayane.karolina@a.ucb.br>
2024-07-27 11:50:44 -0300

    atualizacao cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,13 +2,23 @@
 public class Cliente {
 
 	private String nome;
+	private String cpf;
 
+	public Cliente(String nome, String cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+}
 	public String getNome() {
 		return nome;
 	}
+	
+	public String getCpf() {
+		return cpf;
+}
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+@Override
+public String toString() {
+		return "Cliente: " + nome + ", CPF: " + cpf;
 
+}
 }
